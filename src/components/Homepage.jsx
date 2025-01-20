dccc6ffd2f339d3ba780335c6d82a1f4e13f270c
rebbagopi3 <rebbagopi5@gmail.com>
2025-01-21 01:17:12 +0530

    home page text clr change

--- src/components/Homepage.jsx
+++ src/components/Homepage.jsx
@@ -8,7 +8,7 @@ const Homepage = () => {
         <div className="container p-5 mb-5">
 
 
-          <h1>Home Page</h1>
+          <h1 className='text-danger'>Home Page</h1>
           <p>This is the home page of the website.</p>
           
 
